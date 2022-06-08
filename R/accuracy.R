f53efc5311d2916e3a5861baa6c3225cb5cfde38
Igor <igorfsouza18@gmail.com>
2022-06-08 17:49:50 -0300

    accuracy function

---
+++ R/accuracy.R
@@ -0,0 +1,28 @@
+#' Custom histogram plot
+#'
+#' This function will create a custom histogram
+#'
+#' @import dplyr
+#' @import ggplot2
+#'
+#' @param data A data frame
+#' @param y The variable of interest
+#' @param prob The predicted probability column
+#' @param p1 The minimun probability to consider label1 true
+#' @param p2 The maximun probability to consider label1 true
+#' @param filter A label of interest to filter from y
+#' @param label1 First label to compare with y labels
+#' @param label2 Second label to compare with y labels
+#'
+#' @examples
+#' accuracy(data = data, y = "y", prob = "x", p1 = 0.5, filter = "label1", label1 = "label1", label2 = "label2")
+#'
+#' @export
+accuracy <- function(data, y, prob, p1, p2=1, filter, label1, label2){
+
+  data <- dplyr::filter(data, data[[y]] == filter)
+
+  ac <- mean(ifelse(data[[prob]] > p1 & data[[prob]] < p2, label1, label2) == data[[y]])
+
+  return(ac)
+}
